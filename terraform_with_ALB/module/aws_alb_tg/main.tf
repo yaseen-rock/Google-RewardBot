9d5f24f10d660af4ed5d408de9603cafc2701e5e
tejapearlthoughts <100556645+tejapearlthoughts@users.noreply.github.com>
2023-06-09 17:09:13 +0530

    added 443 alb sg rule

--- terraform_with_ALB/module/aws_alb_tg/main.tf
+++ terraform_with_ALB/module/aws_alb_tg/main.tf
@@ -65,6 +65,13 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
