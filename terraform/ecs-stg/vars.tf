8e6e43eaae30d18698403a17322b5f759e7ce7b1
tejapearlthoughts <100556645+tejapearlthoughts@users.noreply.github.com>
2023-06-28 13:55:22 +0530

    [SKIP CI]

--- terraform/ecs-stg/vars.tf
+++ terraform/ecs-stg/vars.tf
@@ -9,7 +9,7 @@ variable "tf_backend_bucket_name" {
 }
 variable "environment" {
   description = "Deployment Environment"
-  default = "staging1"
+  default = "staging"
 }
 variable "as_max_count" {
   type = number
